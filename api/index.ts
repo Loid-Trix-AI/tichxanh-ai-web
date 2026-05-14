d976ef701e2a1edccf735eeb353d64f2a1dbfce6
NirussVn0 <nirusssimple.vn@gmail.com>
2026-05-14 10:18:59 +0700

    feat: implement server entry point and add catastrophic SSR error normalization handler

--- api/index.ts
+++ api/index.ts
@@ -1,10 +1,6 @@
 // @ts-ignore - dist/server/server.js is a generated file
 import server from '../dist/server/server.js';
 
-export const config = {
-  runtime: 'edge',
-};
-
 export default function (request: Request) {
   return server.fetch(request, {}, {});
 }
